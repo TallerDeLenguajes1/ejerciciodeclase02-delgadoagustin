aca47ed1f421544d6ae0467de0f463b6499a6f24
Agustin Delgado <delgadoagustin147@gmail.com>
2021-09-08 17:47:08 -0300

    clases y algunos metodos

---
+++ cuenta_banco/cuenta_banco/cuenta_corriente_dolares.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cuenta_banco
+{
+    enum TipoDeExtraccion
+    {
+        CajeroHumano = 1,
+        CajeroAutomatico = 2
+    }
+
+    class cuenta_corriente_dolares
+    {
+        string titular;
+        string dni;
+        float dinero;
+        DateTime ultima_extraccion;
+
+        public float Dinero { get => dinero; set => dinero = value; }
+        public string Titular { get => titular; set => titular = value; }
+        public string Dni { get => dni; set => dni = value; }
+
+        void insercion(int monto)
+        {
+            dinero += monto;
+        }
+        void extraccion(int monto, TipoDeExtraccion tipo)
+        {
+            if (monto <= 200 && dinero - monto >= 0 && tipo == TipoDeExtraccion.CajeroAutomatico)
+            {
+                dinero -= monto;
+                Console.WriteLine("Extraccion realizada con exito");
+            }
+            else
+            {
+                Console.WriteLine("No puede ese monto");
+            }
+        }
+
+    }
+}
